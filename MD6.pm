4177110846bbe567a4af59b3c547fbafd07c5e22
Andy Armstrong <andy@hexten.net>
2009-06-18 11:00:05 +0100

    Bump version to 0.09

--- MD6.pm
+++ MD6.pm
@@ -15,11 +15,11 @@ Digest::MD6 - Perl interface to the MD6 Algorithm
 
 =head1 VERSION
 
-This document describes Digest::MD6 version 0.08
+This document describes Digest::MD6 version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 our $HASH_LENGTH = 256;
 
